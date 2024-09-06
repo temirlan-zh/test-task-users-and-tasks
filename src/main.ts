3bef4ae7a57e7d0d3fc48944e5bcb1f53cc18b3f
Temirlan Zhansugurov <temirlan.zhansugurov@gmail.com>
2024-09-06 17:57:49 +0500

    Add centralized exception handling

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Centralized exception handling
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(3000);
 }
 bootstrap();
